9bdf83e66bf65567de97c81fd1cce54e5fadafe8
Nikita Shinov <shinov_96@mail.ru>
2022-01-28 20:09:04 +0300

    new model to Core Data + set up categories VC

---
+++ To-do List/Controllers/CategoryViewController.swift
@@ -0,0 +1,83 @@
+//
+//  CategoryViewController.swift
+//  To-do List
+//
+//  Created by max on 28.01.2022.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+
+    
+    //MARK: - IBActions
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        let addAction = UIAlertAction(title: "Add category", style: .default) { action in
+            //What will happen after user taps to add
+            guard let newTask = textField.text else { return }
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = newTask
+            
+            self.categories.append(newCategory)
+            
+            self.saveCategories()
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+        
+        }
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print ("Error saving context \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print ("Error fetching data from context: \(error)")
+        }
+        tableView.reloadData()
+        
+    }
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.name
+        
+        return cell
+    }
+    
+}
